83b1d446b7863aa4ea2dce115fb405fd677421de
Ganeshvengatesan <ganeshvengatesan01@gmail.com>
2025-10-27 22:57:40 +0530

    Implement inheritance and student marks functionality

--- Inheritance.py
+++ Inheritance.py
@@ -1,38 +1,51 @@
-######### Compile time polymorphism #### 
-class Addition: 
-    def add(self, a, b): 
-        return a + b         
+########  Implementation of Inheritance  ######## 
  
-print("Compile time polymorphism") 
-print("====================") 
-# Create object 
-t1 = Addition() 
-# Using integer arguments 
-print(t1.add(4,5)) 
-# Using string arguments 
-print(t1.add("computer ","science"))  
-# Using list arguments  
-print(t1.add([12,'Williams'],[60,70,'pass']))       
+#### Creating the class student  #### 
+class student: 
+    def get_student_data(self): 
+        self.rollno=int(input("enter rollno")) 
+        self.name=input("enter name") 
+        self.clsname=input("enter class studying")  
  
-######### Run time polymorphism #### 
-class Fruit: 
-    def color(self): 
-        return "Fruits have generally some natural color" 
+    def display_student_data(self): 
+        print("RollNo       : ",self.rollno) 
+        print("StudentName  : ",self.name) 
+        print("Class        : ",self.clsname)  
  
-class Apple(Fruit): 
-    def color(self): 
-        return "Apple is Red in color" 
  
-class Mango(Fruit): 
-    def color(self): 
-        return "Mango is Yellow in color" 
-class Watermelon(Fruit): 
-    def color(self): 
-        return "Watermelon is Green in color" 
  
-print("Run time polymorphism") 
-print("=================") 
-print(Apple().color()) 
-print(Mango().color()) 
-print(Watermelon().color()) 
-print(Fruit().color())
+#### Creating a class mark1 by inheriting the class student #### 
+ 
+class marks(student): 
+     def get_marks_data(self): 
+         self.get_student_data() 
+         self.mark1=int(input("enter mark1"))  
+         self.mark2=int(input("enter mark2"))  
+         if self.mark1>49 and self.mark2>49: 
+              self.result="pass" 
+         else: 
+              self.result="fail"  
+     def display_marks_data(self): 
+        self.display_student_data() 
+        print("Mark1      : ",self.mark1) 
+        print("Mark2      : ",self.mark2) 
+        print("Result     : ",self.result)  
+ 
+###### Creating an object of class student ##### 
+ 
+s1=student() 
+print("Enter the information of the object s1") 
+s1.get_student_data() 
+s1.display_student_data() 
+ 
+##### Creating objects of class marks #### 
+ 
+s2=marks() 
+print("Enter the information of the object s2") 
+s2.get_marks_data() 
+s2.display_marks_data() 
+ 
+s3=marks() 
+print("Enter the information of the object s3") 
+s3.get_marks_data() 
+s3.display_marks_data() 
